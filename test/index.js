da538786c8e34d9333aeb2b653c2f7a32f449bab
michaelrhodes <spam@michaelrhod.es>
2013-11-08 10:18:32 +1000

    Improved test?

--- test/index.js
+++ test/index.js
@@ -3,34 +3,48 @@ var stream = require('stream-mixin')
 var observe = require('../')
 
 run('it works', function(test) {
-  test.plan(2)
-
-  var done = false
-  var model = { key: 'value' }
-  var input = document.createElement('input')
-  input.type = 'checkbox'
-
+  // Setup model
+  var model = { key: 'value' } 
   observe.call(model)
-  stream.call(input)
 
+  // Setup checkbox (ie. some DOM element)
+  var input = document.createElement('input')  
+  input.type = 'checkbox'
   input.readable = true
   input.writable = true
+  
+  stream.call(input)
+
+  // When the model (or whatever is piped
+  // into input) is updated, run this.  
   input.write = function(data) {
     input.value = data.key
-    if (done) {
-      return
-    }
-    done = true
     input.click()
   }
-  
+ 
+  // Make them talk 
   model.pipe(input).pipe(model)
 
-  input.onclick = function() {
+  var handle = function() {
+    input.removeEventListener('click', handle)
+    
+    // The input value should have automatically
+    // updated to match the model.
     test.equal(input.value, 'new value')
-    input.emit('data', { key: 'newer value' })
+
+    // Simulate user input that changes the value.    
+    input.value = 'newer value'
+    input.emit('data', { key: input.value })
+    
+    // The model should have updated to use the
+    // user-defined value.
     test.equal(model.key, 'newer value')
+
+    test.end()
   }
 
+  input.addEventListener('click', handle)
+
+  // Get the ball rolling by updating the model.
   model.key = 'new value'
 })
